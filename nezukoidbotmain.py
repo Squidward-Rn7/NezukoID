482527fbce68b45dab9b3f51398743cf2a5f8c84
Squidward-Rn7 <86432231+Squidward-Rn7@users.noreply.github.com>
2021-10-24 18:27:45 +0700

    Delete nezukoidbotmain.py

--- nezukoidbotmain.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system(
-    "git clone https://github.com/Squidward-Rn7/NezukoID /root/nezukoidbot && mkdir /root/nezukoidbot/bin/ && cd /root/nezukoidbot/ && chmod +x /usr/local/bin/* && python3 -m nezukoidbot"
-)
